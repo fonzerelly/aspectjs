7df2328cc90436e9d2652295f2032101efbd0270
fonzerelly <ChristianHoerauf@web.de>
2014-12-21 01:28:33 +0100

    Interfacedefinition of aspect

--- src/aspect.js
+++ src/aspect.js
@@ -1,2 +1,13 @@
 (function (window) {
+  window.aspect = function(onWhichObjectIsTheMethod, nameOfTheToChangeMethod, exchangeBy) {
+    if (typeof nameOfTheToChangeMethod !== "string") {
+      throw new TypeError("aspect awaits a String for nameOfTheToChangeMethod");
+    }
+    if (onWhichObjectIsTheMethod[nameOfTheToChangeMethod] === undefined) {
+      throw new TypeError("aspects let you only manipulate functions that are part of the passed object");
+    }
+    if (typeof exchangeBy !== "function") {
+      throw new TypeError("aspects awaits a Function for exchangeBy");
+    }
+  };
 }(window));
--- test/aspect.spec.js
+++ test/aspect.spec.js
@@ -1,2 +1,24 @@
 describe("aspectjs", function () {
+
+  describe("aspect", function () {
+    var obj;
+
+    beforeEach(function () {
+      obj = {
+        method: function () {}
+      };
+    });
+
+    it("should be defined", function () {
+      expect(aspect).toBeDefined();
+    });
+    it("should accept only method names in an object", function () {
+      expect(_.partial(aspect, obj, obj.method, function () {})).toThrow();
+      expect(_.partial(aspect, obj, "unkonwnMethod", function() {})).toThrow();
+    });
+    it("should accept only a function as replacement for the objects method", function () {
+      expect(_.partial(aspect, obj, "method", "dummy")).toThrow();
+    });
+  }); //aspect
+
 });
