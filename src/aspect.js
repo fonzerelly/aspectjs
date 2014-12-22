8dbed8a1b310884eb144661240f5a6504574d8ce
fonzerelly <ChristianHoerauf@web.de>
2014-12-22 07:40:08 +0100

    Modified Object implemented - callback pending

--- src/aspect.js
+++ src/aspect.js
@@ -6,8 +6,22 @@
     if (onWhichObjectIsTheMethod[nameOfTheToChangeMethod] === undefined) {
       throw new TypeError("aspects let you only manipulate functions that are part of the passed object");
     }
-    if (typeof exchangeBy !== "function") {
+    if (!(exchangeBy instanceof Function)) {
       throw new TypeError("aspects awaits a Function for exchangeBy");
     }
+    var originalMethod = onWhichObjectIsTheMethod[nameOfTheToChangeMethod];
+
+    onWhichObjectIsTheMethod[nameOfTheToChangeMethod] = function () {
+      var methodArguments = Array.prototype.slice.call(arguments),
+          result = "result";
+      exchangeBy.apply(this, [function () {
+        result = originalMethod();
+      }, methodArguments]);
+      return result;
+    };
+
+    return function () {
+      onWhichObjectIsTheMethod[nameOfTheToChangeMethod] = originalMethod;
+    };
   };
 }(window));
--- test/aspect.spec.js
+++ test/aspect.spec.js
@@ -5,20 +5,101 @@ describe("aspectjs", function () {
 
     beforeEach(function () {
       obj = {
-        method: function () {}
+        method: function () { return "result"; }
       };
     });
 
     it("should be defined", function () {
       expect(aspect).toBeDefined();
     });
-    it("should accept only method names in an object", function () {
-      expect(_.partial(aspect, obj, obj.method, function () {})).toThrow();
-      expect(_.partial(aspect, obj, "unkonwnMethod", function() {})).toThrow();
+
+    describe("interface", function () {
+      it("should accept only method names in an object", function () {
+        expect(_.partial(aspect, obj, obj.method, function () {})).toThrow();
+        expect(_.partial(aspect, obj, "unkonwnMethod", function() {})).toThrow();
+      });
+      it("should accept only a function as replacement for the objects method", function () {
+        expect(_.partial(aspect, obj, "method", "dummy")).toThrow();
+      });
+      it("should return a function", function() {
+        expect(aspect(obj, "method", function () {}) instanceof Function).toBeTruthy();
+      });
+    }); //interface
+
+    describe("manipulated object", function() {
+      it("should have another method", function() {
+        var orgMethod = obj.method;
+        aspect(obj, "method", function () {});
+        expect(obj.method).not.toEqual(orgMethod);
+      });
+      it("should call the passed function instead of the specified method", function () {
+        var spyable = {
+          replacement: function () {}
+        };
+        spyOn(spyable, 'replacement');
+        aspect(obj, "method", spyable.replacement);
+        obj.method();
+        expect(spyable.replacement).toHaveBeenCalled();
+      });
+      it("should call the original method, after aspect got reversed", function () {
+        spyOn(obj, 'method');
+        var reverseAspect = aspect(obj, 'method', function() {});
+        reverseAspect();
+        obj.method();
+        expect(obj.method).toHaveBeenCalled();
+      });
+    }); //manipulated object
+
+    describe("callback", function () {
+      it("should get passed the original function as first parameter", function () {
+        var touchedOriginalMethod = false;
+        obj.method = function () {
+          touchedOriginalMethod = true;
+        };
+
+        aspect(obj, "method", function (first) {
+          first();
+        });
+        obj.method();
+        expect(touchedOriginalMethod).toBeTruthy();
+      });
+      it("should get passed the arguments, that got passed to the org function call as second argument", function () {
+        var paramsToPass = [1,2,3];
+         aspect(obj, "method", function (_, second) {
+           expect(second).toEqual(paramsToPass);
+         });
+         obj.method.apply(obj, paramsToPass);
+      });
+      it("should get passed the object it is attached to as 'this' reference", function () {
+        aspect(obj, "method", function () {
+          expect(this).toBe(obj);
+        });
+      });
     });
-    it("should accept only a function as replacement for the objects method", function () {
-      expect(_.partial(aspect, obj, "method", "dummy")).toThrow();
-    });
+    describe("original method", function () {
+      describe("when callback does not prevent the passage of parameters", function () {
+        it("should return the same result as the original method. A wrapping function can not return anything by return.", function() {
+          var resultOriginalMethod = obj.method();
+          aspect(obj, "method", function(func, funcArgs) {
+           return "this will never be returned by obj.method()";
+          });
+          expect(obj.method()).toEqual(resultOriginalMethod);
+        });
+
+        // it("shoud get the parameters, the callback had been called with", function () {
+        //   var paramsToPass = [1,2,3];
+        //   obj.method = function () {
+        //     var paramsGot = Array.prototype.slice.call(arguments);
+        //     expect(paramsGot.to
+        //   });
+        //   aspect(obj, "method", function (fn, params) {
+        //     fn(params);
+        //   });
+        //   obj.method.apply(obj, paramsToPass);
+        //   expect(obj.method).toHaveBeenCalledWith(1,2,3);
+        // });
+      });
+    }); //callback
   }); //aspect
 
 });
